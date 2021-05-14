77bcccaae73497e962f00c39d7c939f9faecc2ab
mlaizure <maddi.laizure@gmail.com>
2021-05-14 21:16:28 +0000

    Fixed pluralization and semistandard

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -34,11 +34,25 @@ $(function () {
     success: function (data) {
       for (let i = 0; i < data.length; i++) {
         const place = data[i];
-        $('.places').append('<article><div class="title_box"><h2>' + place.name + '</h2><div class="price_by_night">$' + place.price_by_night + '</div></div><div class="information"><div class="max_guest">' + place.max_guest + ' Guest</div><div class="number_rooms">' + place.number_rooms + ' Bedroom</div><div class="number_bathrooms">' + place.number_bathrooms + ' Bathroom</div></div><div class="description">' + place.description + '</div></article>');
+        $('.places').append(
+          '<article><div class="title_box"><h2>' + place.name +
+            '</h2><div class="price_by_night">$' + place.price_by_night +
+            '</div></div><div class="information"><div class="max_guest">' +
+            pluralize(place.max_guest, 'Guest') +
+            '</div><div class="number_rooms">' +
+            pluralize(place.number_rooms, 'Bedroom') +
+            '</div><div class="number_bathrooms">' +
+            pluralize(place.number_bathrooms, 'Bathroom') +
+            '</div></div><div class="description">' +
+            place.description + '</div></article>');
       }
     }
   });
 });
+
+function pluralize (num, name) {
+  if (num === 1) { return `${num} ${name}`; } else { return `${num} ${name}s`; }
+}
 
 /*
 $(".checkbox").change(function() {
