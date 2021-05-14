86b9aedb6c7c3de7d5e028c895c548589ff9b50a
mlaizure <maddi.laizure@gmail.com>
2021-05-14 20:25:29 +0000

    Draft of task 4

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -14,7 +14,7 @@ $(function () {
 
 $(function () {
   $.getJSON(
-    'http://0.0.0.0:5001/api/v1/status/',
+    'http://55.55.55.5:5001/api/v1/status/',
     function (data) {
       if (data.status === 'OK') {
         $('DIV#api_status').addClass('available');
@@ -23,6 +23,38 @@ $(function () {
       }
     }
   );
+});
+
+$(function () {
+  $.ajax({
+    url: 'http://55.55.55.5:5001/api/v1/places_search/',
+    type: 'POST',
+    contentType: 'application/json',
+    data: '{}',
+    success: function (data) {
+      $.each(data, function (place) {
+        $('.places').append(
+	  `<article>
+	     <div class="title_box">
+	       <h2>${place.name}</h2>
+	       <div class="price_by_night">${place.price_by_night}</div>
+	     </div>
+	     <div class="information">
+	       <div class="max_guest">${place.max_guest} Guest</div>
+               <div class="number_rooms">${place.number_rooms} Bedroom</div>
+               <div class="number_bathrooms">${place.number_bathrooms} Bathroom</div>
+	     </div>
+	     <div class="user">
+               <b>Owner:</b> ${place.user.first_name} ${place.user.last_name}
+             </div>
+             <div class="description">
+	       ${place.description}
+             </div>
+	   </article>`
+        );
+      });
+    }
+  });
 });
 
 /*
